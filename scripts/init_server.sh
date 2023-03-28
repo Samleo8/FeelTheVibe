404afe03849ece0d5e3429b439c72591aad4fd24
Samuel Leong <leowhiz@yahoo.com.sg>
2023-03-28 16:07:04 -0400

    condense into single custom block function

---
+++ scripts/init_server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Ports
+PORT=${2:-4446}
+
+# Block Name
+BLOCK_NAME=${1:-"custom"}
+BLOCK_POSTFIX="-processing-block-py"
+
+BLOCK="${BLOCK_NAME}${BLOCK_POSTFIX}"
+
+# Kill any old processes
+fuser -k $PORT/tcp
+killall ngrok
+
+if [ -f server.pid ]; then
+    kill $(cat server.pid)
+fi
+
+# Start server
+cd $BLOCK
+PORT=$PORT python dsp-server.py &
+PID=$!
+cd ..
+
+# Save PID to file to kill later
+echo $PID > server.pid
+
+# Start ngrok
+ngrok http $PORT
