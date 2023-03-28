be5237ae0fa2e0568d953484790a56b79c14b3bd
Samuel Leong <leowhiz@yahoo.com.sg>
2023-03-28 16:23:25 -0400

    combine all features into single py

--- chroma-processing-block-py/dsp.py
+++ chroma-processing-block-py/dsp.py
@@ -10,38 +10,87 @@ def soundDataToFloat(data):
     INT_MAX = 32768
     return (np.array(data) / INT_MAX).astype(np.float32)
 
+
 def generate_features(implementation_version, draw_graphs, raw_data, axes,
-                      sampling_freq, scale_axes):
+                      sampling_freq, use_chroma, use_zcr, use_rms):
+    '''
+    Generate series of features from raw data
+
+    Features from:
+    https://www.kaggle.com/code/shivamburnwal/speech-emotion-recognition?scriptVersionId=34958802&cellId=40
+    '''
+    # Convert raw WAV files in int16 form to float
     raw_data = soundDataToFloat(raw_data)
-
-    # features is a 1D array, reshape so we have a matrix
-    # Chroma_stft
-    # https://librosa.org/doc/main/generated/librosa.feature.chroma_stft.html
     sample_rate = sampling_freq
-    stft = np.abs(librosa.stft(raw_data))
-    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,
-                     axis=0)
 
+    assert use_chroma or use_zcr or use_rms, "At least one feature must be selected"
+
+    # Initialize empty features
+    features = np.empty(0)
     graphs = []
-    if draw_graphs:
-        graphs.append({
-            'name': 'Chroma',
-            'X': np.arange(0, len(chroma)).tolist(),
-            'y': chroma.tolist(),
-        })
+
+    # Chroma STFT
+    # https://librosa.org/doc/main/generated/librosa.feature.chroma_stft.html
+    if use_chroma:
+        stft = np.abs(librosa.stft(raw_data))
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,
+                         axis=0)
+
+        features = np.hstack((features, chroma))
+
+        print("Chroma:", chroma.shape)
+
+        if draw_graphs:
+            graphs.append({
+                'name': 'Chroma',
+                'X': np.arange(0, len(chroma)).tolist(),
+                'y': chroma.tolist(),
+            })
+
+    # Zero Crossing Rate
+    # https://librosa.org/doc/main/generated/librosa.feature.zero_crossing_rate.html
+    if use_zcr:
+        zcr = np.mean(librosa.feature.zero_crossing_rate(raw_data).T, axis=0)
+        features = np.hstack((features, zcr))
+
+        print("ZCR:", zcr.shape)
+
+        if draw_graphs:
+            graphs.append({
+                'name': 'Zero Crossing Rate',
+                'X': np.arange(0, len(zcr)).tolist(),
+                'y': zcr.tolist(),
+            })
+
+    # Root Mean Square
+    # https://librosa.org/doc/main/generated/librosa.feature.rms.html
+    if use_rms:
+        rms = np.mean(librosa.feature.rms(raw_data).T, axis=0)
+        features = np.hstack((features, rms))
+
+        print("RMS:", rms.shape)
+
+        if draw_graphs:
+            graphs.append({
+                'name': 'Root Mean Square',
+                'X': np.arange(0, len(rms)).tolist(),
+                'y': rms.tolist(),
+            })
+
+    print("Features:", features.shape)
 
     return {
-        'features': chroma,
+        'features': features,
         'graphs': graphs,
         # if you use FFTs then set the used FFTs here (this helps with memory optimization on MCUs)
         # NOTE: Unsure if this is correct
-        'fft_used': [stft.tolist()],
+        'fft_used': [] if not use_chroma else [stft.tolist()],
         'output_config': {
             # type can be 'flat', 'image' or 'spectrogram'
             'type': 'flat',
             'shape': {
                 # shape should be { width, height, channels } for image, { width, height } for spectrogram
-                'width': len(chroma),
+                'width': len(features),
             }
         }
     }
--- zcr-rms-processing-block-py/README.md
+++
@@ -1,3 +0,0 @@
-# Custom processing block example (Python)
-
-This is an example of a custom processing block, which you can load in the Edge Impulse studio. See the docs: [Building custom processing blocks](https://docs.edgeimpulse.com/docs/custom-blocks).
