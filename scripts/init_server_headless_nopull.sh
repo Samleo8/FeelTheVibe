398d0ba980ca38956174a188058c4b1a042982c1
Samuel Leong <samleocw@gmail.com>
2023-04-20 23:53:53 -0400

    added no pull script

---
+++ scripts/init_server_headless_nopull.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Kill old processses
+kill_old_procs(){
+    if [ -f server.pid ]; then
+        kill $(cat server.pid)
+    fi
+}
+
+# Ports
+PORT=${2:-4446}
+
+# Block Name
+BLOCK_NAME=${1:-"lpcc"}
+BLOCK_POSTFIX="-processing-block-py"
+
+BLOCK="${BLOCK_NAME}${BLOCK_POSTFIX}"
+
+# Kill any old processes
+kill_old_procs
+
+# Start server
+cd $BLOCK
+PORT=$PORT python3 dsp-server.py &> dsp_server.log &
+PID=$!
+cd ..
+
+echo "Server started at localhost:$PORT -> https://vibe.strasserver.com"
+
+# Save PID to file to kill later
+echo $PID > server.pid
+
+# Kill old processes
+trap kill_old_procs SIGINT
